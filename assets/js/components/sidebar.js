791d28fb9b39ae0f4bb62a9b594d5c260b6a6151
Arkadiusz Plichta <el.pikel@gmail.com>
2024-01-20 20:22:04 +0100

    removed not needed css

--- assets/js/components/sidebar.js
+++ assets/js/components/sidebar.js
@@ -11,11 +11,8 @@ export default function Sidebar() {
     return (
         <aside className="h-screen">
             <nav className={`h-full flex flex-col bg-white border-r shadow-sm ${expanded ? "" : "w-16"}`}>
-                <div className="p-4 pb-2 flex justify-between items-center">
-                    <img
-                        src="/images/logo.svg"
-                        className={`overflow-hidden transition-all ${expanded ? "w-8" : "hidden"}`}>
-                    </img>
+                <div className="p-4 pb-2 flex items-center">
+                    <img src="/images/logo.svg" className={expanded ? "w-8" : "hidden"}></img>
                     <span class={`flex-1 text-lg font-semibold text-indigo-600 ml-5 ${expanded ? "" : "hidden"}`}>Kaverno</span>
                     <button onClick={toggle} className="p-1.5 rounded-lg hover:bg-indigo-50">
                         {expanded ? <span className="hero-chevron-left w-6 h-6"></span> : <span className="hero-chevron-right w-6 h-6"></span>}
@@ -30,7 +27,7 @@ export default function Sidebar() {
 
                 <div className="border-t flex p-3">
                     <img src="https://ui-avatars.com/api/?background=c7d2fe&color=3730a3&bold=true" className="w-10 h-10 rounded-md"></img>
-                    <div className="flex justify-between items-center overflow-hidden transition-all w-52 ml-3">
+                    <div className="flex items-center w-52 ml-3">
                         <div className="leading-4">
                             <h4 className="font-semibold">John Doe</h4>
                             <div className="text-xs text-gray-600">johndoe@gmail.com</div>
